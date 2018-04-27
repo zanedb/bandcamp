837c50c7e146f14d10e0ddf0840cd9139d8210c7
Zane <contact@zane.sh>
2018-04-27 13:34:02 -0700

    Save filename as track name

--- fetch.py
+++ fetch.py
@@ -2,13 +2,21 @@ import requests
 import re
 
 def getStreamUrl(html):
-    return re.findall(r'\"mp3-128\"\w*:\w*\"(.*?)\"', html.text)[0]
+  return re.findall(r'\"mp3-128\"\w*:\w*\"(.*?)\"', html.text)[0]
+
+def getTitle(html):
+  return re.findall(r'\"title\"\w*:\w*\"(.*?)\"', html.text)[0]
 
 url = input('Please enter a Bandcamp song link: ')
 html = requests.get(url)
 stream_url = getStreamUrl(html)
-print(stream_url)
+title = getTitle(html)
+print('URL: '+stream_url)
+print('Title: '+title)
+print('Downloading as "'+title+'.mp3"..')
+file_name = title+'.mp3'
 mp3_file = requests.get(stream_url)
 if mp3_file.status_code == 200:
-  with open("file.mp3", 'wb') as f:
+  with open(file_name, 'wb') as f:
     f.write(mp3_file.content)
+    print('Done!')
