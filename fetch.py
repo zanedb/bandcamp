080d4adaea0ee46a805fddb4f95fbf0554c7ee98
Zane <contact@zane.sh>
2018-04-27 13:26:00 -0700

    Download file

--- fetch.py
+++ fetch.py
@@ -8,3 +8,7 @@ url = input('Please enter a Bandcamp song link: ')
 html = requests.get(url)
 stream_url = getStreamUrl(html)
 print(stream_url)
+mp3_file = requests.get(stream_url)
+if mp3_file.status_code == 200:
+  with open("file.mp3", 'wb') as f:
+    f.write(mp3_file.content)
